77f0584dcfa8d9422173ad47b1272df6873ce077
Andrei Popescu <andreigabrielpopescu556@gmail.com>
2022-07-26 20:37:03 +0300

    Added .env local file example

--- simplified-backend/src/app.ts
+++ simplified-backend/src/app.ts
@@ -1,4 +1,6 @@
-require("dotenv").config();
+require("dotenv").config({
+  path: "./.env.local",
+});
 
 import express, { NextFunction, Request, Response } from "express";
 import morgan from "morgan";
