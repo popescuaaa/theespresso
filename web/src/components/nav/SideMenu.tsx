8f650ccfb278119c123ba35fa70a5c0856938993
Daniel Bana <f.danielflorinel.bana@gmail.com>
2022-08-20 13:03:44 +0300

    Small modifications on side menu

--- web/src/components/nav/SideMenu.tsx
+++ web/src/components/nav/SideMenu.tsx
@@ -7,12 +7,12 @@ const SideMenu = () => {
     const location = useLocation();
     const path = location.pathname;
 
-    const [isExpanded, setIsExpanded] = useState(false);
+    const [expanded, setExpanded] = useState(false);
 
     return (
-        <div className={"side-menu" + (isExpanded ? " side-menu-expanded" : "")}>
+        <div className={"side-menu" + (expanded ? " side-menu-expanded" : "")}>
             <div className="side-menu-header">
-                <button onClick={() => setIsExpanded(!isExpanded)} className={"btn-trigger" + (isExpanded ? " btn-trigger-expanded" : "")}>
+                <button onClick={() => setExpanded(!expanded)} className={"btn-trigger" + (expanded ? " btn-trigger-expanded" : "")}>
                     &gt;&gt;
                 </button>
                 <div className="side-menu-header-title">
@@ -24,7 +24,7 @@ const SideMenu = () => {
                     return (
                         <div className="side-menu-body-item" key={index}>
                             <div className="side-menu-body-item-title">
-                                <Link to={item.path} className={path === item.path ? "active" : ""}>
+                                <Link onClick={() => setExpanded(false)} to={item.path} className={path === item.path ? "active" : ""}>
                                     {item.name}
                                 </Link>
                             </div>
